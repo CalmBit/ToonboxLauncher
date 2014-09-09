3667183170497f4c33c4a2bcd8499731fc31382f
xPythonic <gr33nmyth@aol.com>
2014-09-09 08:06:29 -0400

    ui: Add basic QT UI core

---
+++ src/core/main.cpp
@@ -0,0 +1,13 @@
+#include "ui/MainWindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+	QApplication application(argc, argv);
+
+	MainWindow *main_window = new MainWindow;
+	main_window->show();
+
+	return application.exec();
+}
