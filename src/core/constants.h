42ebb15666654b664f4437af1623ae81555c96e1
xPythonic <gr33nmyth@aol.com>
2014-09-13 02:47:50 -0400

    core: Add DISTRIBUTION_TOKEN and DOWNLOAD_SERVER_URL to constants.h

--- src/core/constants.h
+++ src/core/constants.h
@@ -1,5 +1,8 @@
 #pragma once
 #include <string>
 
+const std::string DISTRIBUTION_TOKEN = "test/win32";
+
 const std::string REPORT_A_BUG_URL = "https://launchpad.net/toontowninfinite";
 const std::string HOME_PAGE_URL = "https://toontowninfinite.com";
+const std::string DOWNLOAD_SERVER_URL = "http://download.toontowninfinite.com";
