bd75280f6111d25728076600408aa2e4548258e9
xPythonic <gr33nmyth@aol.com>
2014-09-13 13:08:48 -0400

    core: Store constants in QStrings rather than std::strings

--- src/core/constants.h
+++ src/core/constants.h
@@ -1,11 +1,16 @@
 #pragma once
-#include <string>
+#include <QString>
 
-const std::string VERSION = "1.0.3";
-const std::string DISTRIBUTION_TOKEN = "test/win32";
+const QString VERSION = "1.0.3";
+const QString DISTRIBUTION = "test";
+const QString PLATFORM = "win32";
+const QString DISTRIBUTION_TOKEN = "test/win32";
 
-const std::string REPORT_A_BUG_URL = "https://launchpad.net/toontowninfinite";
-const std::string HOME_PAGE_URL = "https://toontowninfinite.com";
-const std::string DOWNLOAD_SERVER_URL = "http://download.toontowninfinite.com";
+const QString USER_AGENT = "TTI-Launcher";
 
-const std::string ERROR_OUT_OF_DATE = "Your launcher is out of date. Please download and install the most recent one via the Toontown Infinite website.";
+const QString URL_REPORT_A_BUG = "https://launchpad.net/toontowninfinite";
+const QString URL_HOME_PAGE = "https://toontowninfinite.com";
+const QString URL_DOWNLOAD_SERVER = "http://download.toontowninfinite.com";
+const QString URL_ACCOUNT_SERVER_ENDPOINT = "https://toontowninfinite.com/api/login/";
+
+const QString ERROR_OUT_OF_DATE = "Your launcher is out of date. Please download and install the most recent one via the Toontown Infinite website.";
